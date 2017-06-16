9260668e391c3b92adf8e421272610ca3bac3ed8
melampyge <ozer.duman@gmail.com>
2017-06-16 21:02:49 +0200

    Plotting function for end-to-end distance calculation.

---
+++ E2E_collective/plot_e2e.py
@@ -0,0 +1,149 @@
+
+""" Plot average end-to-end vector of filaments"""
+
+### example command line arguments: 
+
+##############################################################################
+
+import sys
+sys.path.append('../Utility')
+
+import argparse
+import numpy as np
+import os
+import matplotlib as mpl
+mpl.use('Agg', warn=False)
+import matplotlib.pyplot as plt
+import misc_tools
+import read_write
+
+##############################################################################
+
+def get_args():
+    """ get context arguments"""
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-fl", "--folder", nargs="?", \
+                        const='/local/duman/SIMULATIONS/many_polymers_5/density_0.2/', \
+                        help="Folder containing data, as in /local/duman/SIMULATIONS/many_polymers_5/density_0.2/") 
+    parser.add_argument("-af", "--analysisfile", nargs="?", const="RE2E", \
+                        help="Address of the analysis file as in RE2E")     
+    parser.add_argument("-sb", "--savebase", nargs="?", \
+                        const = "/usr/users/iff_th2/duman/RolfData/PLOTS/", \
+                        help="Folder to save the data, as in /usr/users/iff_th2/duman/RolfData/PLOTS/") 
+    parser.add_argument("-sf", "--savefolder", nargs="?", const="E2E", \
+                        help="Specific folder for saving, as in E2E")      
+    args = parser.parse_args()
+    
+    return args
+
+##############################################################################
+
+def read_e2e_data(path):
+    """ read specific analysis data"""
+    
+    fl = open(path, 'r')
+    
+    fl.readline()               # comment line
+    fl.readline()               # empty line
+    
+    line = fl.readline()
+    ll = line.split()
+    e2e = float(ll[-1])
+    
+    line = fl.readline()
+    ll = line.split()
+    std = float(ll[-1])
+    
+    fl.close()
+    
+    return e2e, std
+
+##############################################################################
+
+def plot_data(xp, yp, sims, savebase, savefolder, param_choice):
+    """ plot the data,
+    NOTE THAT xp and yp are dictionaries with keys as chosen parameter,
+    and values as the x and y axis of the plot"""
+    
+    ### set general plot properties
+
+    sim = sims[sims.keys()[0]]
+    #downlim = -1
+    #uplim = sim.lx/4.
+    num_ticks = 5
+    ax_len = 1.0                          # Length of one subplot square box
+    ax_b = 0.0                            # Beginning/offset of the subplot in the box
+    ax_sep = 0.0                          # Separation length between two subplots
+    total_subplots_in_x = 1               # Total number of subplots    
+    fig = plt.figure()
+    subp = misc_tools.Subplots(fig, ax_len, ax_sep, ax_b, total_subplots_in_x) 
+    ax0 = subp.addSubplot()
+ 
+    ### save properties
+    
+    base = savebase + savefolder + '/'
+    os.system("mkdir -p " + base)  
+    savepath = base + '/' + savefolder + '_' + param_choice + '.pdf'
+    
+    for key in xp.keys():
+        
+        x = xp[key]
+        y = yp[key]
+        length = sim.length
+        
+        label = '$\\xi_{p}/L=$' + str(key)
+        line0 = ax0.loglog(x, y/sim.length**2, \
+                         linewidth=2.0, label=label)
+    
+    ### title
+    
+#    ax0.set_title("$t/\\tau_{D}$ = " + "{0:.2f}".format(time/sim.tau_D) + \
+#        ", $t/\\tau_{A}$ = " + "{0:.2f}".format(time/sim.tau_A), fontsize=30)
+    
+    ### labels
+
+    ax0.set_xlabel("$Pe$", fontsize=40)
+    ax0.set_ylabel("$\langle \xi_r^2\rangle/L^2$", fontsize=40)
+
+    ### limits
+
+    #ax0.set_xlim((-1, 15))
+    #ax0.set_ylim((downlim, uplim))
+    
+    ### ticks
+    
+    #ax0.xaxis.set_ticks(np.linspace(0, 15, num_ticks, endpoint=True))
+    #ax0.yaxis.set_ticks(np.linspace(0, uplim, num_ticks, endpoint=True))
+    ax0.tick_params(axis='both', which='major', labelsize=30)
+    
+    ### legend
+
+    ax0.legend(bbox_to_anchor=(1.005, 0.,0.65, 1.), loc=2, borderaxespad=0., \
+        prop={'size': 20}, mode="expand", frameon=False)
+    
+    ### save 
+    
+    plt.savefig(savepath, dpi=300, bbox_inches='tight', pad_inches=0.08)        
+    fig.clf()                          
+        
+    return
+
+##############################################################################
+
+def main():
+    
+    args = get_args()
+    param_choice = 'kappa'          # plot with kappa as the legend
+    x, y, sims = misc_tools.collect_data(args.folder, args.analysisfile, 
+                                         read_e2e_data, param_choice)
+    plot_data(x, y, sims, args.savebase, args.savefolder, param_choice)
+
+    return
+
+##############################################################################
+
+if __name__ == '__main__':
+    main()
+    
+##############################################################################    
